105afc6889b02395f05f0f08c919f5458530081e
edizcelik <mr.edizcelik@gmail.com>
2024-01-26 23:23:25 -0800

    Add whoami route

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,19 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static("public"));
 
 app.get("/api/whoami", (req: Request, res: Response) => {
-  res.json({ hello: "whoami" });
+  try {
+    const software = req.headers["user-agent"];
+    const language = req.headers["accept-language"];
+    const ipaddress =
+      req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
+
+    res.json({ software, language, ipaddress });
+  } catch (error) {
+    res.status(500);
+    res.json({
+      message: "Something went wrong.",
+    });
+  }
 });
 
 app.listen(port, () => {
